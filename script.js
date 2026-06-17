bf5b325564c0e286d66e5664c1d8c3be51b41223
yatheeshbobby <yatheeshg@thestackly.com>
2026-06-18 05:28:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+var employeename = "G.Yatheesh";
+let employeeage = "32";
+const companyname = "stackly";
+
+console.log("employeename:",employeename);
+console.log("employeeage:",employeeage);
+console.log("companyname:",companyname);
+
+alert("welcome to employee portal");
+let taskcompleted =confirm("have you completed today's task?");
+
+console.log(taskcompleted);
+
+let employeename = prompt("enter your name");
+
+console.log(employeenamr);
+
+console.warn("task submitted successfully");
+
+console.error("task submission faield");
+
+// employeeage before promotion
+employeeage = 31;
+console.log("employee age before promotion:", employeeage);
+
+// update age after promotion
+employeeage =32;
+console.log("employee age after promotion:", employeeage);
+
+
